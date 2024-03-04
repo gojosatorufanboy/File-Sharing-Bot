a6fd1229b61056e9f77ba58bb68faf694c81f1f8
Gojo <101966680+gojosatorufanboy@users.noreply.github.com>
2024-03-04 17:33:03 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -125,6 +125,11 @@ async def not_joined(client: Client, message: Message):
             InlineKeyboardButton(
                 "Join Channel",
                 url = client.invitelink)
+[
+InlineKeyboardButton(
+                "Join Channel",
+                url = https://t.me/Frindlybots )
+       ] 
         ]
     ]
     try:
